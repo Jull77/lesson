bc595bf23758c98fc4b886a3bca635c82b39bd1d
Julia <julia.lav1996@mail.ru>
2022-08-31 23:32:08 +0300

    Add Task47

---
+++ Task47/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, 
+// заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5  7   -2    -0,2
+// 1  -3,3   8    -9,9
+// 8   7,8  -7,1   9
+
+Console.WriteLine("Введите количество строк m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[,] array = new double[m, n];
+FillMatrixRandomArray(array);
+PrintMatrix(array);
+
+void FillMatrixRandomArray(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+        }
+    }
+}
+
+void PrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j], 7},");
+            else Console.Write($"{matrix[i, j], 7}");
+        }
+        Console.WriteLine("]");
+    }
+}
