29b1a2b368b594c6732e0481d35a2b9c2e12a047
Julia <julia.lav1996@mail.ru>
2022-09-01 23:20:13 +0300

    Add Task52+/-

---
+++ Task52/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+
+int row = 3;
+int col = 4;
+int[,] num = new int[row, col];
+FillArray2D(num);
+PrintArray2D(num);
+
+double[] avgNum = new double[num.GetLength(1)];
+
+for (int i = 0; i < num.GetLength(1); i++)
+{
+    double result = 0.0;
+    for (int j = 0; j < num.GetLength(0); j++)
+    {
+        result += num[j, i];
+    }
+    avgNum[i] = result / num.GetLength(0);
+}
+PrintArray(avgNum);
+
+void FillArray2D(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray2D(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void PrintArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+
+
