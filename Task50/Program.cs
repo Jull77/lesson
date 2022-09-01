833aa3e7d7fc6ed1d764b0ab176ce8de27550868
Julia <julia.lav1996@mail.ru>
2022-09-01 17:13:20 +0300

    Add Task50+/-

---
+++ Task50/Program.cs
@@ -0,0 +1,65 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает 
+// позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же 
+// указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1, 7 -> такого элемента в массиве нет
+
+Console.WriteLine("Введите число, определяющее строку элемента: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число, определяющее столбец элемента: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int m2 = m - 1;
+int n2 = n - 1;
+
+int[,] CreateMatrixRndInt(int row, int col, int min, int max)
+{
+    int[,] matrix = new int[row, col];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4},");
+            else Console.Write($"{matrix[i, j],4}");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+void PrintElement(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) 
+        {
+            if (i < m && j < n) Console.WriteLine($"Элемент с заданными значениями: {matrix[m2, n2]}");
+            else Console.WriteLine($"Нет элемента с заданными значениями"); 
+        }
+    }   
+   
+}
+
+
+int[,] matrix = CreateMatrixRndInt(3, 4, -99, 99);
+PrintMatrix(matrix);
+PrintElement(matrix);
+
